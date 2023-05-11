c80324f6229a43c805097cd1d9e5f07518d8cecf
Viktor <vincet1998@gmail.com>
2023-05-11 21:13:13 +0300

    lab1_2A add UI elems,add clicks buttons,toast

--- app/src/main/java/com/myapplication/astontwo/MainActivity.kt
+++ app/src/main/java/com/myapplication/astontwo/MainActivity.kt
@@ -10,12 +10,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btn_lab1_1 = findViewById<Button>(R.id.btn_lab1_1)
+        val btn_lab1_2A = findViewById<Button>(R.id.btn_lab1_2a)
         btn_lab1_1.setOnClickListener {
             startActivityForLab1_1()
+        }
+        btn_lab1_2A.setOnClickListener {
+            startActivityForLab1_2A()
         }
     }
     fun startActivityForLab1_1(){
         val intent = Intent(this,CodeLabs1_1Activity::class.java)
+        startActivity(intent)
+    }
+    fun startActivityForLab1_2A(){
+        val intent = Intent(this,CodeLabs1_2AActivity::class.java)
         startActivity(intent)
     }
 }
