1d4bcb9ac6691ab96c9a1da1daa3bf6084ffca60
Viktor <vincet1998@gmail.com>
2023-05-11 10:18:02 +0300

    codelabs 1.1

--- app/src/main/java/com/myapplication/astontwo/MainActivity.kt
+++ app/src/main/java/com/myapplication/astontwo/MainActivity.kt
@@ -2,10 +2,40 @@ package com.myapplication.astontwo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("ActivityCycle","create")
         setContentView(R.layout.activity_main)
+        val tv = findViewById<TextView>(R.id.tvmain)
+        tv.text = "Happy Bithday!"
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("ActivityCycle","resume")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("ActivityCycle","start")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("ActivityCycle","stop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("ActivityCycle","destroy")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("ActivityCycle","pause")
     }
 }
