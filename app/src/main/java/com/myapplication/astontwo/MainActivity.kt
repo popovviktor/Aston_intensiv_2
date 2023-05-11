6a0c9dbd2e2dea07ee0052d2a4b4c80fa6bdf8dd
Viktor <vincet1998@gmail.com>
2023-05-11 10:46:40 +0300

    codelabs 1.2A

--- app/src/main/java/com/myapplication/astontwo/MainActivity.kt
+++ app/src/main/java/com/myapplication/astontwo/MainActivity.kt
@@ -3,15 +3,30 @@ package com.myapplication.astontwo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var mcount = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("ActivityCycle","create")
         setContentView(R.layout.activity_main)
-        val tv = findViewById<TextView>(R.id.tvmain)
+        val tv = findViewById<TextView>(R.id.show_count)
+        val btn_count = findViewById<Button>(R.id.button_count)
+        val btn_toast = findViewById<Button>(R.id.button_toast)
         tv.text = "Happy Bithday!"
+        tv.text = mcount.toString()
+
+        btn_toast.setOnClickListener {
+            Toast.makeText(this,R.string.toast_message,Toast.LENGTH_LONG).show()
+        }
+        btn_count.setOnClickListener {
+            mcount++
+            tv.text = mcount.toString()
+        }
+
     }
 
     override fun onResume() {
